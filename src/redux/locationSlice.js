bcf1ed487b088501d94271a30774b7b0d0847d51
shivasakte <skate_92@ymail.com>
2022-03-24 23:42:14 +0800

    Moved all api calls to go through async thunk.

--- src/redux/locationSlice.js
+++ src/redux/locationSlice.js
@@ -1,11 +1,15 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
+import Geocode from "react-geocode";
+
 const initialState = {
   SuggestedLocations: [],
-  SelectedLocation: ''
+  SelectedLocation: null,
+  CurrentPosition: {lat:0, lng:0}
 };
 
 const autoComplete = new window.google.maps.places.AutocompleteService();
+Geocode.setApiKey(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
 
 export const fetchLocations = createAsyncThunk(
     'locations/PlacePredictions',
@@ -14,6 +18,31 @@ export const fetchLocations = createAsyncThunk(
         return response.predictions;
     }
   )
+
+export const getCurrentLocation = createAsyncThunk(
+    'locations/CurrentPosition',
+    async () => {
+          const position = await getPosition();
+          const CurrentPosition = { lat:position.coords.latitude, lng:position.coords.longitude}
+          return CurrentPosition;
+    }
+)
+
+const getPosition = () => {
+  return new Promise((response,reject)=>{
+    navigator.geolocation.getCurrentPosition(response,reject);
+  })
+}
+
+export const setCurrentLocationByGeo = createAsyncThunk(
+  'locations/GeoUpdateLocation',
+  async (selectedAddress) => {
+    const position = await Geocode.fromAddress(selectedAddress)
+    const { lat, lng } = position.results[0].geometry.location;
+    const CurrentPosition = { lat, lng }
+    return CurrentPosition;
+  }
+)
 
 export const locationSlice = createSlice({
   name: 'location',
@@ -38,6 +67,14 @@ export const locationSlice = createSlice({
     builder.addCase(fetchLocations.fulfilled, (state, action) => {
       // Add user to the state array
       state.SuggestedLocations = action.payload;
+    })
+    builder.addCase(getCurrentLocation.fulfilled, (state, action) => {
+      // Add user to the state array
+      state.CurrentPosition = action.payload;
+    })
+    builder.addCase(setCurrentLocationByGeo.fulfilled, (state, action) => {
+      // Add user to the state array
+      state.CurrentPosition = action.payload;
     })
   },
 })
